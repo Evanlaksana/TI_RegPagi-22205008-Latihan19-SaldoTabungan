c9fe92461a5b12481c7eae6562ab6abc7abcc5dc
Evanlaksana <132762464+Evanlaksana@users.noreply.github.com>
2023-10-28 17:17:02 +0700

    Add files via upload

---
+++ Latihan19/src/latihan19/Latihan19.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan19;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Latihan19 {
+
+        public static void main(String[] args) {
+         double saldoAwal = 2500000;
+        double bungaBulan = 15;
+        int lamaBulan = 6;
+
+        // Hitung bunga per bulan
+        double bungaPerBulan = saldoAwal * bungaBulan / 100;
+
+        // Hitung saldo akhir setiap bulan
+        double saldoAkhir = saldoAwal;
+        for (int i = 1; i <= lamaBulan; i++) {
+            saldoAkhir += bungaPerBulan;
+            System.out.println("Saldo di bulan ke-" + i + " Rp." + saldoAkhir);
+        }
+    }
+}
+    
+    
+
